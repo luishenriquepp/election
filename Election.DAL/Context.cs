f631629e8424a595ecfca5eeec94bab409334fa8
luishenriquepp <peppyno@gmail.com>
2017-01-16 13:06:01 -0200

    Creating the EntityFramework context

---
+++ Election.DAL/Context.cs
@@ -0,0 +1,12 @@
+﻿using Election.Models;
+using System.Data.Entity;
+
+namespace Election.DAL
+{
+    public class Context : DbContext
+    {
+        public IDbSet<Restaurant> Restaurant { get; set; }
+        public IDbSet<Poll> Election { get; set; }
+        public IDbSet<Vote> Vote { get; set; }
+    }
+}
--- Election.Models/Election.cs
+++ Election.Models/Election.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Election.Models
 {
-    public class Votacao
+    public class Poll
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
