b3ece9066bf47d6d2a61b5a756f7cd434ba9dd5b
luishenriquepp <peppyno@gmail.com>
2017-01-18 13:45:29 -0200

    Changing the default view. Now it shows when the election is over.

--- Election/AngularApp/Controllers/poll.js
+++ Election/AngularApp/Controllers/poll.js
@@ -1,31 +1,40 @@
 ﻿(function (app) {
     app.controller('pollController', ['$scope', 'pollFactory','voteFactory', function ($scope, pollFactory, voteFactory) {
-        $scope.title = "Selecione um restaurante";
+        $scope.title = 'Escolha aonde você quer almoçar hoje!';
+        $scope.subtitle = 'Restaurantes disponíveis';
+        $scope.btnValue = 'Selecione um restaurante';
         $scope.disabled = true;
         $scope.enabled = true;
         $scope.selected = '';
         $scope.vote = { 'Id': 0, 'RestaurantId': 0, 'UserToken': 0, 'Poll_Id': 0 };
         $scope.available = [];
 
-        $scope.availableRestaurants = [];
         $scope.load = function () {
             pollFactory.init()
             .success(function (data) {
-                $scope.enabled = !data.Voted && canVote();
+                $scope.enabled = showButton(data.Voted, data.WinnerId, canVote());
                 $scope.available = data.ElectionVotes;
+                if (data.WinnerId) {
+                    $scope.title = 'Votação encerrada.';
+                    $scope.available.forEach(function (el) {
+                        if (el.Restaurant.Id == data.WinnerId) {
+                            $scope.subtitle = 'Restaurante vencedor: ' + el.Restaurant.Name;
+                            return;
+                        }
+                    })
+                }
             });
         };
         $scope.click = function (restaurant) {
-            $scope.title = "Votar em " + restaurant.Name;
+            $scope.btnValue = 'Votar em ' + restaurant.Name;
             $scope.disabled = false;
             $scope.selected = restaurant.Id;
             $scope.vote.RestaurantId = restaurant.Id;
         }
         $scope.votar = function() {
-            console.log($scope.vote);
             voteFactory.create($scope.vote)
             .success(function (data) {
-                $scope.voted = true;
+                $scope.enabled = false;
                 $scope.available.forEach(function (el) {
                     if (el.Restaurant.Id == $scope.vote.RestaurantId) {
                         el.Votes++;
@@ -35,8 +44,17 @@
         }
         var canVote = function () {
             var date = new Date();
-            if (date.getHours() >= 7 && date.getHours() < 12)
+            if (date.getHours() >= 7 && date.getHours() < 13)
                 return true;
+        }
+        var showButton = function (hasVoted, winnerId, canvote) {
+            if (winnerId) {
+                return false;
+            } else if (hasVoted) {
+                return false;
+            } else if (canVote()) {
+                return true;
+            }
         }
     }]);
 }(angular.module('electionApp')));
