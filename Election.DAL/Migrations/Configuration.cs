525404fb0b6f13344d3a6c3c24afaa68fef685fc
luishenriquepp <peppyno@gmail.com>
2017-01-19 17:41:54 -0200

    Including 5 restaurants on Seed method.

--- Election.DAL/Migrations/Configuration.cs
+++ Election.DAL/Migrations/Configuration.cs
@@ -8,23 +8,18 @@ namespace Election.DAL.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Restaurant.AddOrUpdate(
+                new Restaurant { Id = 1, Name = "McDonalds", Adress = "Rua Silva Só", Neighborhood = "Centro " },
+                new Restaurant { Id = 2, Name = "Burguer King", Adress = "Bento Gonçalves", Neighborhood = "Santa Marta" },
+                new Restaurant { Id = 3, Name = "Subway", Adress = "Avenida Praia de Belas", Neighborhood = "Praia de Belas" },
+                new Restaurant { Id = 4, Name = "Bobs", Adress = "Avenida dos Carvalhos", Neighborhood = "Santana" },
+                new Restaurant { Id = 5, Name = "Cenoura Pastéis", Adress = "Rua dos Parques", Neighborhood = "Praiana" }
+            );
         }
     }
 }
